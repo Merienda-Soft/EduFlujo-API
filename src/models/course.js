81ea19919e46778b3d31cf49b0a3a71f25aac0fc
DORIAN ESTEFAN CHOQUE CALDERON <ccd0029570@est.univalle.edu>
2024-10-23 23:43:52 -0400

    Gestion de Entorno

--- src/models/course.js
+++ src/models/course.js
@@ -5,13 +5,23 @@ const CourseSchema = new Schema({
   name: { 
     type: String, 
     required: true 
-},
+  },
+  deleted: { 
+    type: Number, 
+    required: false,
+    default: 0
+  },
+  status: { 
+    type: Number, 
+    required: false,
+    default: 0
+  },
   materias: [
     { 
         type: mongoose.Schema.Types.ObjectId, 
         ref: 'materias' 
     }
-]  
+  ]  
 });
 
 module.exports = mongoose.model('cursos', CourseSchema);
