19711032387312ed48218bb0f279e60857481fbc
DORIAN ESTEFAN CHOQUE CALDERON <ccd0029570@est.univalle.edu>
2025-05-08 14:53:10 -0400

    refix index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,7 @@
 import { HttpServer } from './shared/http/server';
 import apiRouter from './modules/api.routes';
-import { Router } from 'express';
 
 const server = new HttpServer();
 
-// Endpoint de Hola Mundo en la raíz
-const rootRouter = Router();
-rootRouter.get('/', (req, res) => {
-  res.json({ message: '¡Hola Mundo!' });
-});
-
-server.addRoute('/', rootRouter);
 server.addRoute('/api', apiRouter);
 server.start();
